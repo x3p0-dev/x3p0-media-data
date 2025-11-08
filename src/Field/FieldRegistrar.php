8998326013c4f1ef72c0cd51286e61382f3aa0da
Justin Tadlock <justintadlock@gmail.com>
2025-11-08 17:44:58 -0600

    Add constants for each of the field keys.

--- src/Field/FieldRegistrar.php
+++ src/Field/FieldRegistrar.php
@@ -18,40 +18,64 @@ namespace X3P0\MediaData\Field;
  */
 final class FieldRegistrar
 {
+	public const ALBUM = 'album';
+	public const APERTURE = 'aperture';
+	public const ARTIST = 'artist';
+	public const ASPECT_RATIO = 'aspect_ratio';
+	public const CAMERA = 'camera';
+	public const CREATED_TIMESTAMP = 'created_timestamp';
+	public const DIMENSIONS = 'dimensions';
+	public const DISPLAY_ORIENTATION = 'display_orientation';
+	public const FILE_SIZE = 'file_size';
+	public const FILE_NAME = 'file_name';
+	public const FOCAL_LENGTH = 'focal_length';
+	public const GENRE = 'genre';
+	public const ISO = 'iso';
+	public const LENGTH_FORMATTED = 'length_formatted';
+	public const MIME_TYPE = 'mime_type';
+	public const ORIENTATION = 'orientation';
+	public const SHUTTER_SPEED = 'shutter_speed';
+	public const TITLE = 'title';
+	public const TRACK_NUMBER = 'track_number';
+	public const YEAR = 'year';
+
 	/**
 	 * An array of field keys and their associated classes, to be stored in
 	 * the field registry.
 	 */
-	private const FIELDS = [
-		'album'               => Type\Album::class,
-		'aperture'            => Type\Aperture::class,
-		'artist'              => Type\Artist::class,
-		'aspect_ratio'        => Type\AspectRatio::class,
-		'camera'              => Type\Camera::class,
-		'created_timestamp'   => Type\CreatedTimestamp::class,
-		'dimensions'          => Type\Dimensions::class,
-		'display_orientation' => Type\DisplayOrientation::class,
-		'file_size'           => Type\FileSize::class,
-		'file_name'           => Type\FileName::class,
-		'focal_length'        => Type\FocalLength::class,
-		'genre'               => Type\Genre::class,
-		'iso'                 => Type\Iso::class,
-		'length_formatted'    => Type\LengthFormatted::class,
-		'mime_type'           => Type\MimeType::class,
-		'orientation'         => Type\ExifOrientation::class,
-		'shutter_speed'       => Type\ShutterSpeed::class,
-		'title'               => Type\Title::class,
-		'track_number'        => Type\TrackNumber::class,
-		'year'                => Type\Year::class
-	];
+	private static function getFields(): array
+	{
+		return [
+			self::ALBUM               => Type\Album::class,
+			self::APERTURE            => Type\Aperture::class,
+			self::ARTIST              => Type\Artist::class,
+			self::ASPECT_RATIO        => Type\AspectRatio::class,
+			self::CAMERA              => Type\Camera::class,
+			self::CREATED_TIMESTAMP   => Type\CreatedTimestamp::class,
+			self::DIMENSIONS          => Type\Dimensions::class,
+			self::DISPLAY_ORIENTATION => Type\DisplayOrientation::class,
+			self::FILE_SIZE           => Type\FileSize::class,
+			self::FILE_NAME           => Type\FileName::class,
+			self::FOCAL_LENGTH        => Type\FocalLength::class,
+			self::GENRE               => Type\Genre::class,
+			self::ISO                 => Type\Iso::class,
+			self::LENGTH_FORMATTED    => Type\LengthFormatted::class,
+			self::MIME_TYPE           => Type\MimeType::class,
+			self::ORIENTATION         => Type\ExifOrientation::class,
+			self::SHUTTER_SPEED       => Type\ShutterSpeed::class,
+			self::TITLE               => Type\Title::class,
+			self::TRACK_NUMBER        => Type\TrackNumber::class,
+			self::YEAR                => Type\Year::class
+		];
+	}
 
 	/**
 	 * Registers default fields with the registry.
 	 */
 	public static function register(FieldRegistry $fieldRegistry): void
 	{
-		foreach (self::FIELDS as $key => $fieldClass) {
-			$fieldRegistry->register($key, $fieldClass);
+		foreach (self::getFields() as $key => $className) {
+			$fieldRegistry->register($key, $className);
 		}
 	}
 }
