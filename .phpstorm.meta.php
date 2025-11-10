692bf80125460af945a1093fdef8ab2baa5fec64
Justin Tadlock <justintadlock@gmail.com>
2025-11-10 14:45:23 -0600

    Add PhpStorm meta file.

---
+++ .phpstorm.meta.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * PhpStorm metadata, particularly for the service container.
+ *
+ * @author    Justin Tadlock <justintadlock@gmail.com>
+ * @copyright Copyright (c) 2025, Justin Tadlock
+ * @license   https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0-or-later
+ * @link      https://github.com/x3p0-dev/x3p0-media-data
+ */
+
+declare (strict_types = 1);
+
+namespace PHPSTORM_META
+{
+	// For get() method.
+	override(\X3P0\MediaData\Core\Container::get(0), map(['' => '@']));
+
+	// For make() method.
+	override(\X3P0\MediaData\Core\Container::make(0), map(['' => '@']));
+}
